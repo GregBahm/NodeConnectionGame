f20d6ba045174e5bee00c1d562309470aa05f015
Greg Bahm <grbahm@microsoft.com>
2024-11-24 19:42:22 -0800

    Sketching the geometry

--- NodeGameUnity/Assets/ConnectionBehavior.cs
+++ NodeGameUnity/Assets/ConnectionBehavior.cs
@@ -8,12 +8,56 @@ public class ConnectionBehavior : MonoBehaviour
     [SerializeField]
     private LineRenderer lineRenderer;
 
-    private const int SEGMENT_COUNT = 10;
+    private const int SEGMENT_COUNT = 100;
+
+    private void Start()
+    {
+        lineRenderer.positionCount = SEGMENT_COUNT;
+    }
 
     public void Place(NodeState from, NodeState to)
     {
-        Vector2 offset = to.Pos - from.Pos;
-        transform.localPosition = new Vector3(from.Pos.x, 0, from.Pos.y);
-        lineRenderer.SetPosition(1, new Vector3(offset.x, 0, offset.y));
+        Vector2 fromPerpendicular = new Vector2(-from.Direction.y, from.Direction.x);
+        Vector2 toPerpendicular = new Vector2(-to.Direction.y, to.Direction.x);
+        
+        Vector2? maybeIntersection = GetLineLineIntersection(from.Pos, fromPerpendicular, to.Pos, toPerpendicular);
+
+        if(!maybeIntersection.HasValue)
+        {
+            return;
+        }
+        Vector2 intersection = maybeIntersection.Value;
+        transform.localPosition = new Vector3(intersection.x, 0, intersection.y);
+
+        float circleRadius = (from.Pos - intersection).magnitude;
+
+        //TODO: ReplaceThisMath
+
+        float fromAngle = Vector2.SignedAngle(Vector2.up, from.Pos - intersection);
+        float toAngle = Vector2.SignedAngle(Vector2.up, to.Pos - intersection);
+
+        for (int i = 0; i < SEGMENT_COUNT; i++)
+        {
+            float t = i / (float)(SEGMENT_COUNT - 1);
+            float angle = Mathf.Lerp(0, 360, t);//Mathf.Lerp(fromAngle, toAngle, t);
+            Vector2 pos = intersection + circleRadius * new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+            lineRenderer.SetPosition(i, new Vector3(pos.x, 0, pos.y));
+        }
+    }
+
+    public static Vector2? GetLineLineIntersection(Vector2 lineAStart, Vector2 lineADirection, Vector2 lineBStart, Vector2 lineBDirection)
+    {
+        float crossDir = lineADirection.x * lineBDirection.y - lineADirection.y * lineBDirection.x;
+
+        if (Mathf.Abs(crossDir) < Mathf.Epsilon)
+        {
+            return null; // No intersection
+        }
+
+        Vector2 diffStart = lineBStart - lineAStart;
+        float t = (diffStart.x * lineBDirection.y - diffStart.y * lineBDirection.x) / crossDir;
+
+        Vector2 intersection = lineAStart + t * lineADirection;
+        return intersection;
     }
 }
---
+++ NodeGameUnity/Assets/GeometryTesterScene.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeometryTesterScene : MonoBehaviour
+{
+    [SerializeField]
+    private Transform fromTransform;
+    [SerializeField]
+    private Transform toTransform;
+
+    [SerializeField]
+    private LineRenderer fromCircleLineRenderer;
+    [SerializeField]
+    private LineRenderer toCircleLineRenderer;
+
+    [SerializeField]
+    private Transform intersectionPoint;
+
+    private const int SEGMENT_COUNT = 100;
+
+    private void Start()
+    {
+        fromCircleLineRenderer.positionCount = SEGMENT_COUNT;
+        toCircleLineRenderer.positionCount = SEGMENT_COUNT;
+    }
+
+    private void Update()
+    {
+        NodeState from = GetNodeFromTransform(fromTransform);
+        NodeState to = GetNodeFromTransform(toTransform);
+
+        Vector2 fromPerpendicular = new Vector2(-from.Direction.y, from.Direction.x);
+        Vector2 toPerpendicular = new Vector2(-to.Direction.y, to.Direction.x);
+
+        Vector2? maybeIntersection = GetLineLineIntersection(from.Pos, fromPerpendicular, to.Pos, toPerpendicular);
+
+        if (!maybeIntersection.HasValue)
+        {
+            return;
+        }
+        Vector2 intersection = maybeIntersection.Value;
+        intersectionPoint.localPosition = new Vector3(intersection.x, 0, intersection.y);
+
+        DrawCircle(fromCircleLineRenderer, from, intersection);
+        DrawCircle(toCircleLineRenderer, to, intersection);
+    }
+
+    private void DrawCircle(LineRenderer renderer, NodeState pointOnCircle, Vector2 center)
+    {
+        float circleRadius = (pointOnCircle.Pos - center).magnitude;
+        for (int i = 0; i < SEGMENT_COUNT; i++)
+        {
+            float t = i / (float)(SEGMENT_COUNT - 1);
+            float angle = Mathf.Lerp(0, 360, t);//Mathf.Lerp(fromAngle, toAngle, t);
+            Vector2 pos = center + circleRadius * new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+            renderer.SetPosition(i, new Vector3(pos.x, 0, pos.y));
+        }
+    }
+
+    private NodeState GetNodeFromTransform(Transform node)
+    {
+        Vector2 pos = new Vector2(node.position.x, node.position.z);
+        Vector2 direction = new Vector2(node.forward.x, node.forward.z);
+        return new NodeState() { Pos =pos, Direction = direction };
+    }
+
+    public static Vector2? GetLineLineIntersection(Vector2 lineAStart, Vector2 lineADirection, Vector2 lineBStart, Vector2 lineBDirection)
+    {
+        float crossDir = lineADirection.x * lineBDirection.y - lineADirection.y * lineBDirection.x;
+
+        if (Mathf.Abs(crossDir) < Mathf.Epsilon)
+        {
+            return null; // No intersection
+        }
+
+        Vector2 diffStart = lineBStart - lineAStart;
+        float t = (diffStart.x * lineBDirection.y - diffStart.y * lineBDirection.x) / crossDir;
+
+        Vector2 intersection = lineAStart + t * lineADirection;
+        return intersection;
+    }
+}
