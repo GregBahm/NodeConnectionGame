182ceeb1a15feefc5a14aef927cffbad8480d21f
Greg Bahm <grbahm@microsoft.com>
2024-12-03 22:09:28 -0800

    A series of rake handles to the face

--- NodeGameUnity/Assets/GeometryTesting/ArcBehavior.cs
+++ NodeGameUnity/Assets/GeometryTesting/ArcBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Properties;
@@ -5,24 +6,19 @@ using UnityEngine;
 
 public class ArcBehavior : MonoBehaviour
 {
-    public Vector2 LineStart { get; set; }
-    public Vector2 LineEnd { get; set; }
-    public Vector2 ArcStart { get; set; }
-    public Vector2 ArcEnd { get; set; }
+    public NodeState LineStart { get; set; }
 
-    public Vector2 Center { get; set; }
+    public NodeState LineEnd { get; set; }
+
+    public Vector2 ArcStart { get; private set; }
+    public Vector2 ArcEnd { get; private set; }
+
+    public Vector2 Center { get; private set; }
 
     public Color ColorStart { get; set; }
     public Color ColorEnd { get; set; }
 
-    public float BaseAngleOffset { get; private set; }
-
     private int resolution = 100;
-
-    public float Sweep { get; private set; }
-
-    public bool LongWayAround { get; set; }
-    public bool CounterClockwise { get; set; }
 
     [SerializeField]
     private LineRenderer theRenderer;
@@ -35,34 +31,116 @@ public class ArcBehavior : MonoBehaviour
     }
     private void Update()
     {
-
-        float circleRadius = (ArcStart - Center).magnitude;
-        Sweep = Vector2.Angle(ArcStart - Center, ArcEnd - Center);
-        BaseAngleOffset = Vector2.SignedAngle(Vector2.right, ArcStart - Center);
-        float angleOffset = BaseAngleOffset;
-
-        if(LongWayAround)
+        Vector2? maybeIntersection = GetLineLineIntersection(LineStart.Pos, LineStart.Direction, LineEnd.Pos, LineEnd.Direction);
+        if (!maybeIntersection.HasValue)
         {
-            Sweep = 360 - Sweep;
+            //TODO: Handle this case
+            return;
         }
 
-        theRenderer.SetPosition(0, new Vector3(LineStart.x, 0, LineStart.y));
+        SetArcStartAndEnd(maybeIntersection.Value);
+
+        float sweep = GetSweep();
+        float circleRadius = (ArcStart - Center).magnitude;
+        float angleOffset = Vector2.SignedAngle(Vector2.right, ArcStart - Center);
+
+        SetTheRenderer(sweep, circleRadius, angleOffset);
+    }
+
+    private void SetArcStartAndEnd(Vector2 intersection)
+    {
+
+        Vector2 theBisector = (LineStart.Direction + LineEnd.Direction).normalized;
+        theBisector = Vector2.Perpendicular(theBisector);
+
+        Vector2 fromPerpendicularDir = Vector2.Perpendicular(LineStart.Direction);
+        Vector2 toPerpendicularDir = Vector2.Perpendicular(LineEnd.Direction);
+
+        float fromToIntersection = (LineStart.Pos - intersection).magnitude;
+        float toToIntersection = (LineEnd.Pos - intersection).magnitude;
+
+        Vector2 offsetFromCenter = GetLineLineIntersection(LineStart.Pos, fromPerpendicularDir, intersection, theBisector).Value;
+        Vector2 offsetToCenter = GetLineLineIntersection(LineEnd.Pos, toPerpendicularDir, intersection, theBisector).Value;
+
+        Vector2 offsetEnd = ReflectPointOverLine(LineStart.Pos, intersection, theBisector);
+        Vector2 offsetStart = ReflectPointOverLine(LineEnd.Pos, intersection, theBisector);
+
+        float offsetDot = Vector2.Dot(offsetStart - LineStart.Pos, LineStart.Direction);
+        if (offsetDot > 0)
+        {
+            ArcStart = LineStart.Pos;
+            ArcEnd = offsetEnd;
+        }
+        else
+        {
+            ArcStart = offsetStart;
+            ArcEnd = LineEnd.Pos;
+        }
+        Center = offsetToCenter;
+    }
+
+    private static Vector2 ReflectPointOverLine(Vector2 point, Vector2 lineStart, Vector2 lineDirection)
+    {
+        Vector2 pointToStart = point - lineStart;
+        float projectionLength = Vector2.Dot(pointToStart, lineDirection);
+        Vector2 projection = projectionLength * lineDirection;
+        Vector2 closestPointOnLine = lineStart + projection;
+        Vector2 reflectedPoint = closestPointOnLine * 2 - point;
+        return reflectedPoint;
+    }
+
+    private void SetTheRenderer(float sweep, float circleRadius, float angleOffset)
+    {
+        theRenderer.SetPosition(0, new Vector3(LineStart.Pos.x, 0, LineStart.Pos.y));
         for (int i = 1; i < resolution - 1; i++)
         {
             float t = (i - 1) / (float)(resolution - 3);
-            if(CounterClockwise)
-            {
-                t = -t;
-            }
-            float angle = Sweep * t + angleOffset;
+            float angle = sweep * t + angleOffset;
             Vector2 pos = GetPointAtAngle(Center, angle, circleRadius);
             theRenderer.SetPosition(i, new Vector3(pos.x, 0, pos.y));
         }
-        theRenderer.SetPosition(resolution - 1, new Vector3(LineEnd.x, 0, LineEnd.y));
+        theRenderer.SetPosition(resolution - 1, new Vector3(LineEnd.Pos.x, 0, LineEnd.Pos.y));
+    }
+
+    public static Vector2? GetLineLineIntersection(Vector2 lineAStart, Vector2 lineADirection, Vector2 lineBStart, Vector2 lineBDirection)
+    {
+        float crossDir = lineADirection.x * lineBDirection.y - lineADirection.y * lineBDirection.x;
+
+        if (Mathf.Abs(crossDir) < Mathf.Epsilon)
+        {
+            return null; // No intersection
+        }
+
+        Vector2 diffStart = lineBStart - lineAStart;
+        float t = (diffStart.x * lineBDirection.y - diffStart.y * lineBDirection.x) / crossDir;
+
+        Vector2 intersection = lineAStart + t * lineADirection;
+        return intersection;
+    }
+
+    float GetSweep()
+    {
+        float sweep = Vector2.Angle(ArcStart - Center, ArcEnd - Center);
+
+        Vector2 arcStartToArcEnd = ArcStart - ArcEnd;
+        bool longWayAround = Vector2.Dot(LineStart.Direction, arcStartToArcEnd) < 0;
+        sweep = longWayAround ? 360 - sweep : sweep;
+
+        bool clockwise = IsTangentClockwise(Center, ArcStart, LineStart.Direction);
+        return clockwise ? sweep : -sweep;
     }
 
     private Vector2 GetPointAtAngle(Vector2 circleCenter, float angle, float radius)
     {
         return circleCenter + radius * new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+    }
+
+    public static bool IsTangentClockwise(Vector2 circleCenter, Vector2 tangentPointOnCircle, Vector2 directionOfTangent)
+    {
+        Vector2 centerToPoint = tangentPointOnCircle - circleCenter;
+        Vector2 normalizedTangent = directionOfTangent.normalized;
+
+        float crossProduct = centerToPoint.x * normalizedTangent.y - centerToPoint.y * normalizedTangent.x;
+        return crossProduct < 0;
     }
 }
--- NodeGameUnity/Assets/GeometryTesting/GeometryTesterTwo.cs
+++ NodeGameUnity/Assets/GeometryTesting/GeometryTesterTwo.cs
@@ -15,9 +15,6 @@ public class GeometryTesterTwo : MonoBehaviour
     private GameObject arcPrefab;
 
     [SerializeField]
-    private Color rayColor;
-
-    [SerializeField]
     private Color fromColor;
 
     [SerializeField]
@@ -40,96 +37,13 @@ public class GeometryTesterTwo : MonoBehaviour
         return arc;
     }
 
-    [SerializeField]
-    private Transform debuggyBuddy;
-
     private void Update()
     {
         NodeState from = GetNodeFromTransform(fromTransform);
         NodeState to = GetNodeFromTransform(toTransform);
 
-        Vector2? maybeIntersection = GetLineLineIntersection(from.Pos, from.Direction, to.Pos, to.Direction);
-        if (!maybeIntersection.HasValue)
-        {
-            return;
-        }
-        Vector2 intersection = maybeIntersection.Value;
-
-        Vector2 theBisector = (from.Direction + to.Direction).normalized;
-        theBisector = Vector2.Perpendicular(theBisector);
-
-        Vector2 fromPerpendicularDir = Vector2.Perpendicular(from.Direction);
-        Vector2 toPerpendicularDir = Vector2.Perpendicular(to.Direction);
-
-        float fromToIntersection = (from.Pos - intersection).magnitude;
-        float toToIntersection = (to.Pos - intersection).magnitude;
-
-        Vector2 offsetFromCenter = GetLineLineIntersection(from.Pos, fromPerpendicularDir, intersection, theBisector).Value;
-        Vector2 offsetToCenter = GetLineLineIntersection(to.Pos, toPerpendicularDir, intersection, theBisector).Value;
-
-        Vector2 offsetEnd = ReflectPointOverLine(from.Pos, intersection, theBisector);
-        Vector2 offsetStart = ReflectPointOverLine(to.Pos, intersection, theBisector);
-
-        float offsetDot = Vector2.Dot(offsetStart - from.Pos, from.Direction);
-        if(offsetDot > 0)
-        {
-            theArc.ArcStart = from.Pos;
-            theArc.ArcEnd = offsetEnd;
-            theArc.Center = offsetFromCenter;
-
-            debuggyBuddy.position = new Vector3(offsetEnd.x, 0, offsetEnd.y);
-            debuggyBuddy.rotation = toTransform.rotation;
-        }
-        else
-        {
-            theArc.ArcStart = offsetStart;
-            theArc.ArcEnd = to.Pos;
-            theArc.Center = offsetToCenter;
-
-            debuggyBuddy.position = new Vector3(offsetStart.x, 0, offsetStart.y);
-            debuggyBuddy.rotation = fromTransform.rotation;
-        }
-
-        theArc.LineStart = from.Pos;
-        theArc.LineEnd = to.Pos;
-
-        Vector2 arcStartToArcEnd = theArc.ArcStart - theArc.ArcEnd;
-        longWayAround = Vector2.Dot(from.Direction, arcStartToArcEnd) < 0;
-
-        clockwise = IsTangentClockwise(theArc.Center, theArc.ArcStart, from.Direction);
-
-        theArc.LongWayAround = longWayAround;
-        theArc.CounterClockwise = !clockwise;
-    }
-    public static bool IsTangentClockwise(Vector2 circleCenter, Vector2 tangentPointOnCircle, Vector2 directionOfTangent)
-    {
-        Vector2 centerToPoint = tangentPointOnCircle - circleCenter;
-        Vector2 normalizedTangent = directionOfTangent.normalized;
-        
-        float crossProduct = centerToPoint.x * normalizedTangent.y - centerToPoint.y * normalizedTangent.x;
-        return crossProduct < 0;
-    }
-
-    [SerializeField]
-    private bool longWayAround;
-    [SerializeField]
-    private bool clockwise;
-
-    private static Vector2 ReflectPointOverLine(Vector2 point, Vector2 lineStart, Vector2 lineDirection)
-    {
-        Vector2 pointToStart = point - lineStart;
-        float projectionLength = Vector2.Dot(pointToStart, lineDirection);
-        Vector2 projection = projectionLength * lineDirection;
-        Vector2 closestPointOnLine = lineStart + projection;
-        Vector2 reflectedPoint = closestPointOnLine * 2 - point;
-        return reflectedPoint;
-    }
-
-    private static Vector2 ProjectPointOnLine(Vector2 point, Vector2 lineStart, Vector2 lineDirection)
-    {
-        Vector2 pointToStart = point - lineStart;
-        float dot = Vector2.Dot(pointToStart, lineDirection);
-        return lineStart + dot * lineDirection;
+        theArc.LineStart = from;
+        theArc.LineEnd = to;
     }
 
     private NodeState GetNodeFromTransform(Transform node)
@@ -137,21 +51,5 @@ public class GeometryTesterTwo : MonoBehaviour
         Vector2 pos = new Vector2(node.position.x, node.position.z);
         Vector2 direction = new Vector2(node.forward.x, node.forward.z);
         return new NodeState() { Pos = pos, Direction = direction };
-    }
-
-    public static Vector2? GetLineLineIntersection(Vector2 lineAStart, Vector2 lineADirection, Vector2 lineBStart, Vector2 lineBDirection)
-    {
-        float crossDir = lineADirection.x * lineBDirection.y - lineADirection.y * lineBDirection.x;
-
-        if (Mathf.Abs(crossDir) < Mathf.Epsilon)
-        {
-            return null; // No intersection
-        }
-
-        Vector2 diffStart = lineBStart - lineAStart;
-        float t = (diffStart.x * lineBDirection.y - diffStart.y * lineBDirection.x) / crossDir;
-
-        Vector2 intersection = lineAStart + t * lineADirection;
-        return intersection;
     }
 }
